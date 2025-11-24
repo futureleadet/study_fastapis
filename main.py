5f5116aaa0abf5aee836432849446c6f72ed32be
futureleadet <futureleadet@gmail.com>
2025-11-24 13:24:32 +0900

    ..

--- main.py
+++ main.py
@@ -30,4 +30,10 @@ templates = Jinja2Templates(directory="templates/")
 async def main_html(request: Request):
     return templates.TemplateResponse("main.html"
                                       , {"request": request})
+# http://localhost:8000/main_html
+@app.get("main_page.html")
+async def main_html(request: Request):
+    return templates.TemplateResponse("main_page.html"
+                                      , {"request": request})
+
 pass
