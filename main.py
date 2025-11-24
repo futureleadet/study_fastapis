a626629699ff1081999be6180e48d67a30bb70d5
futureleadet <futureleadet@gmail.com>
2025-11-24 14:17:13 +0900

    ..

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from fastapi import FastAPI
 app = FastAPI()
-# http://localhost:8000/
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
-# http://localhost:8000/html
-@app.get("/html")
-async def root_html():
-    html_content = '''
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Otter</title>
-        </head>
-        <body>
-            <div>My name is Otter!</div>
-        </body>
-        </html>
-        '''
-    return html_content
+# # http://localhost:8000/
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello, World!"}
+# # http://localhost:8000/html
+# @app.get("/html")
+# async def root_html():
+#     html_content = '''
+#         <!DOCTYPE html>
+#         <html lang="en">
+#         <head>
+#             <meta charset="UTF-8">
+#             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#             <title>Otter</title>
+#         </head>
+#         <body>
+#             <div>My name is Otter!</div>
+#         </body>
+#         </html>
+#         '''
+#     return html_content
 
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
@@ -35,6 +35,12 @@ async def main_html(request: Request):
 @app.get("/main_page.html")
 async def main_page_html(request: Request):
     return templates.TemplateResponse("main_page.html"
+                                      , {"request": request})
+
+# http://localhost:8000/templates/restaurant_info.html
+@app.get("/restaurant_info.html")
+async def restaurant_info_html(request: Request):
+    return templates.TemplateResponse("restaurant_info.html"
                                       , {"request": request})
 
 pass
