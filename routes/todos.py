a60bc6f21daff82aca53c8f81f14c24430daee00
futureleadet <futureleadet@gmail.com>
2025-11-25 16:16:01 +0900

    ..

--- routes/todos.py
+++ routes/todos.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from services.db import get_db_connection
+from psycopg2.extras import DictCursor
 
 router = APIRouter()
 
@@ -8,7 +10,16 @@ templates = Jinja2Templates(directory="templates/")
 #http://localhost:8000/todos/
 @router.get("/")
 def get_todos_html(request: Request):
+    conn = get_db_connection()
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+    # with conn.cursor() as cursor:
+        cursor.execute("""SELECT id , item
+                        FROM todo;""")
+        todos = cursor.fetchall()
+    conn.close()
+
     context = {
-        "request" : request
+        "request" : request,
+        "todos" : todos
     }
     return templates.TemplateResponse("todos/merged_todo.html", context)
