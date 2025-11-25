c81ef96b00e899270ab14c02d41da64540c4814b
futureleadet <futureleadet@gmail.com>
2025-11-25 15:03:39 +0900

    ..

--- querys/todos.sql
+++ querys/todos.sql
@@ -3,3 +3,16 @@ CREATE TABLE IF NOT EXISTS todo (
             id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
             item VARCHAR(255) NOT NULL
         )
+
+INSERT INTO todo (item)
+VALUES ('Learn SQL'), ('Build a REST API'), ('Write Unit Tests');
+
+SELECT id , item
+FROM todo;
+
+UPDATE todo
+SET item = 'learn Advanced SQL'
+WHERE id = '47c7adef-dc75-48d6-b705-10b6c035d728';
+
+DELETE FROM todo
+WHERE id = '0bf21a4b-836e-42b8-b9c1-35b5b084fe76';
